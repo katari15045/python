514af26249aecd660613afcf14a31121cdaf96a8
Saketh Katari <katari15045@iiitd.ac.in>
2018-08-13 23:14:40 +0530

    ds : Adds set

---
+++ ds/1_set/main.py
@@ -0,0 +1,11 @@
+
+# set internally uses Hash table, O(1) for getting an element, insertion order is not preserved
+nums = set()
+nums.add(20)
+nums.add(22)
+nums.add(21)
+print(nums)
+
+# iterate through set
+for num in nums:
+    print(num)
